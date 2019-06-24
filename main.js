5a748049eff8e0c3ab65e68341ccf2d66248ee00
Islam Albadri <46619765+gobadri@users.noreply.github.com>
2019-06-24 02:57:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function openAbout(){
+    document.getElementById('aboutcontainer').style.left = '0';
+}
+
+function closeAbout(){
+    document.getElementById('aboutcontainer').style.left = '-2000px';
+}
+
+
+function openContact(){
+    document.getElementById('contactcontainer').style.left = '0';
+}
+
+function closeContact(){
+    document.getElementById('contactcontainer').style.left = '-2000px';
+}
+
+
+function openPortfolio(){
+    document.getElementById('portfoliocontainer').style.left = '0';
+}
+
+function closePortfolio(){
+    document.getElementById('portfoliocontainer').style.left = '-2000px';
+}
